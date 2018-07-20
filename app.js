8d2cc0dcf8bc5eb5cc3ab9dda3fd3f577c4c7978
macun.mc <macun.mc@alibaba-inc.com>
2018-07-20 19:59:40 +0800

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+App({
+  onLaunch(options) {
+    console.log('App Launch', options);
+    console.log('getSystemInfoSync', dd.getSystemInfoSync());
+    console.log('SDKVersion', dd.SDKVersion);
+    this.globalData.corpId = options.query.corpId;
+  },
+  onShow() {
+    console.log('App Show');
+  },
+  onHide() {
+    console.log('App Hide');
+  },
+  globalData: {
+    //填上开放者后台看见的corpId
+    corpId:'***'
+  }
+});
---
+++ page/index/index.js
@@ -0,0 +1,62 @@
+let app = getApp();
+//替换成开发者后台设置的安全域名
+let url = "http://open.dingtalk.com";
+
+//内网穿透工具介绍:
+// https://open-doc.dingtalk.com/microapp/debug/ucof2g
+//let url = "http://your_sub_domain.vaiwan.com/";
+
+Page({
+    data:{
+        corpId: '',
+        authCode:'',
+        userId:''
+    },
+    onLoad(){
+
+        let _this = this;
+
+        this.setData({
+            corpId: app.globalData.corpId
+        })
+
+         
+        dd.getCorpAuthCode({
+            success:(res)=>{
+                _this.setData({
+                    authCode:res.authCode
+                })
+                
+                dd.httpRequest({
+                    url: url+'/login',
+                    method: 'POST',
+                    data: {
+                        authCode: res.authCode
+                    },
+                    dataType: 'json',
+                    success: function(res) {
+                        console.log('success----',res)
+                        let userId = res.data.result.userId;
+                        _this.setData({
+                            userId:userId
+                        })
+                    },
+                    fail: function(res) {
+                        console.log("httpRequestFail---",res)
+                       dd.alert({content: JSON.stringify(res)});
+                    },
+                    complete: function(res) {
+                        dd.hideLoading();
+                    }
+                    
+                });
+            },
+            fail: (err)=>{
+                dd.alert({
+                    content: JSON.stringify(err)
+                })
+            }
+        })
+        
+    }
+})
