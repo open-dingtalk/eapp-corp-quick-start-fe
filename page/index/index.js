ad0a4ef7bbbc1bcc43ca2af0f3b1798fee15205d
macun.mc <macun.mc@alibaba-inc.com>
2018-07-28 23:10:01 +0800

    update

--- page/index/index.js
+++ page/index/index.js
@@ -4,7 +4,7 @@ let app = getApp();
 //内网穿透工具介绍:
 // https://open-doc.dingtalk.com/microapp/debug/ucof2g
 //替换成开发者后台设置的安全域名
-let url = "http://mcadomain.vaiwan.com";
+let url = "http://******";
 
 Page({
     data:{
@@ -21,7 +21,7 @@ Page({
                 this.setData({
                     authCode:res.authCode
                 })
-                
+                //dd.alert({content: "step1"});
                 dd.httpRequest({
                     url: url+'/login',
                     method: 'POST',
@@ -30,7 +30,7 @@ Page({
                     },
                     dataType: 'json',
                     success: (res) => {
-                        //dd.alert({content: "step2"});
+                        // dd.alert({content: "step2"});
                         console.log('success----',res)
                         let userId = res.data.result.userId;
                         let userName = res.data.result.userName;
@@ -51,7 +51,7 @@ Page({
                 });
             },
             fail: (err)=>{
-                //dd.alert({content: "step3"});
+                // dd.alert({content: "step3"});
                 dd.alert({
                     content: JSON.stringify(err)
                 })
